28f00764ac5eb07c9258106a95bed5cf370d96e8
Isr <35469435+isrgish@users.noreply.github.com>
2023-11-06 07:56:01 -0500

    Changed BoardConfig.mk to be more like stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,17 +30,25 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a75
+#changing 3 lines as per stock BoardConfig
+#TARGET_CPU_VARIANT := cortex-a75
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := generic
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
+#changing 2 lines as per stock BoardConfig
+#TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a75
+#changing 3 lines as per stock BoardConfig
+#TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a9
 
 # MY ADD
 # APEX
-#OVERRIDE_TARGET_FLATTEN_APEX := true
+OVERRIDE_TARGET_FLATTEN_APEX := true
 
 TARGET_USES_64_BIT_BINDER := true
 TARGET_SUPPORTS_64_BIT_APPS := true
@@ -58,45 +66,101 @@ TARGET_USES_UEFI := true
 TARGET_SCREEN_DENSITY := 400
 
 # Kernel
-BOARD_KERNEL_CMDLINE := \
-    androidboot.console=ttyMSM0 \
-    androidboot.hardware=qcom \
-    androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3 \
-    lpm_levels.sleep_disabled=1 \
-    msm_rtb.filter=0x237 \
-    firmware_class.path=/vendor/firmware_mnt/image \
-    service_locator.enable=1 \
-    swiotlb=2048 \
-    video=vfb:640x400,bpp=32,memsize=3072000
-
-BOARD_KERNEL_IMAGE_NAME := kernel
-BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE          := 0x00000000
-BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
-BOARD_KERNEL_OFFSET        := 0x00008000
-BOARD_KERNEL_SECOND_OFFSET := 0x00000000  # MY CHANGE from 0x00f00000
+# Changing the cmdline to the stock cmdline
+#BOARD_KERNEL_CMDLINE := \
+#    androidboot.console=ttyMSM0 \
+#    androidboot.hardware=qcom \
+#    androidboot.memcg=1 \
+#    androidboot.usbcontroller=a600000.dwc3 \
+#    lpm_levels.sleep_disabled=1 \
+#    msm_rtb.filter=0x237 \
+#    firmware_class.path=/vendor/firmware_mnt/image \
+#    service_locator.enable=1 \
+#    swiotlb=2048 \
+#    video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 \
+			earlycon=msm_geni_serial,0xa90000 \
+			androidboot.hardware=qcom \
+			androidboot.console=ttyMSM0 \
+			androidboot.memcg=1 \
+			lpm_levels.sleep_disabled=1 \
+			video=vfb:640x400,bpp=32,memsize=3072000 \
+			msm_rtb.filter=0x237 \
+			service_locator.enable=1 \
+			androidboot.usbcontroller=a600000.dwc3 \
+			swiotlb=2048 \
+			loop.max_part=7 \
+			cgroup.memory=nokmem,nosocket \
+			reboot=panic_warm \
+			buildvariant=user
+
+BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET       := 0x01000000
-BOARD_DTB_OFFSET           := 0x01f00000
+BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
+
+# Following lines not in stock
 TARGET_KERNEL_ARCH := arm64
 TARGET_NO_KERNEL := false
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+
+# Original order & flags
+#BOARD_KERNEL_OFFSET        := 0x00008000
+#BOARD_KERNEL_SECOND_OFFSET := 0x00000000  # MY CHANGE from 0x00f00000 (from carliv kitchen unpack)
+#BOARD_DTB_OFFSET           := 0x01f00000
+#BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+#BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+#BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+#BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+#BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+#BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
+#BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+#BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+#BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+# New order & flags as in stock
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := kernel
+# Added following 4 lines from stock
+TARGET_KERNEL_CONFIG := TB-9707F_defconfig
+TARGET_KERNEL_SOURCE := kernel/lenovo/TB-9707F
+
+# Kernel - prebuilt
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+# Following line not in stock
+BOARD_INCLUDE_RECOVERY_DTBO := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 104857600
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_SUPER_PARTITION_GROUPS := lenovo_dynamic_partitions
+BOARD_LENOVO_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    system \
+    system_ext \
+    vendor \
+    product \
+    odm
+BOARD_LENOVO_DYNAMIC_PARTITIONS_SIZE := 91226112200
+# Following 3 lines not in stock
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := 4096
+BOARD_USES_PRODUCTIMAGE := true
 
 # Platform
 TARGET_BOARD_PLATFORM := kona
+# Following 3 lines not in stock
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
 TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
 QCOM_BOARD_PLATFORMS += kona
@@ -105,38 +169,19 @@ QCOM_BOARD_PLATFORMS += kona
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 
 # Partition Info
-BOARD_FLASH_BLOCK_SIZE := 262144
-BOARD_USES_PRODUCTIMAGE := true
-
-BOARD_BOOTIMAGE_PARTITION_SIZE := 104857600
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
-BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
-BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := 4096
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
+# Not in stock
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
-# Dynamic/Logical Partitions
-BOARD_SUPER_PARTITION_SIZE := 9126805504
-BOARD_SUPER_PARTITION_GROUPS := lenovo_dynamic_partitions
-BOARD_LENOVO_DYNAMIC_PARTITIONS_SIZE := 91226112200
-BOARD_LENOVO_DYNAMIC_PARTITIONS_PARTITION_LIST := \
-    system \
-    system_ext \
-    vendor \
-    product \
-    odm
 
 # Partitions (listed in the file) to be wiped under recovery.
 TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery.wipe
 
-# Workaround for error copying vendor files to recovery ramdisk
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
 
 # Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+# Following lines not in stock
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_NO_RECOVERY := false
@@ -154,11 +199,19 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
+# Following lines added from stock for now commenting out
+## Verified Boot
+#BOARD_AVB_ENABLE := true
+#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Encryption
-PLATFORM_VERSION := 127
+# Changed following line to stock
+#PLATFORM_VERSION := 127
+PLATFORM_VERSION := 16.1.0
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
+# Following lines not in stock
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
@@ -170,21 +223,26 @@ PRODUCT_ENFORCE_VINTF_MANIFEST := true
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := false
+# From stock it is set to true
+#TW_INCLUDE_REPACKTOOLS := true
 
 # TWRP specific build flags
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+# Added following line from stock
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+# The rest of the lines are not in stock
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_DEFAULT_BRIGHTNESS := 125
+TW_DEFAULT_BRIGHTNESS :=  100
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_ENCRYPTED_BACKUPS := false
 TW_EXCLUDE_TWRPAPP := true
-TW_EXTRA_LANGUAGES := true
 TW_HAS_EDL_MODE := true
 TW_INCLUDE_NTFS_3G := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_NO_BIND_SYSTEM := true
 TW_NO_EXFAT_FUSE := true
 TW_RECOVERY_ADDITIONAL_RELINK_BINARY_FILES += \
